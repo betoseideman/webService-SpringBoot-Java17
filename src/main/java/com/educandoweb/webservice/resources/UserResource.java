3ae55477473416af99b029fb3535afd5e706bcbc
Beto Seideman <betoseideman@gmail.com>
2022-01-19 10:51:11 -0300

    User Insert

--- src/main/java/com/educandoweb/webservice/resources/UserResource.java
+++ src/main/java/com/educandoweb/webservice/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.webservice.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.webservice.entities.User;
 import com.educandoweb.webservice.services.UserService;
@@ -19,7 +23,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@GetMapping //GET do HTTP
+	@GetMapping //GET do HTTP  GET OBETM
 	public ResponseEntity<List<User>> findAll(){         //TIPO QUE RETORNA RESPOSTAS
 		List<User> list = service.findAll(); 
 		return ResponseEntity.ok().body(list);
@@ -28,6 +32,13 @@ public class UserResource {
 	public ResponseEntity<User> findById(@PathVariable Long id){ // PARA ACEITAR O PARAMENTRO COLOCAR ANOTAÇÃO
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping // POST CONFIGURA // SETAR // INSERIR
+	public ResponseEntity<User> insert(@RequestBody User obj){ //  USAR A @ PARA DIZER QUE VAI CONVERTER DE JSON PARA uSER OO
+		obj = service.insert(obj); //CRIAR UM URI PARA RETORNAR NO JSOM O 201 QUE SIGNIFICA CRIAÇÃO
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 	
 
